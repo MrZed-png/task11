777ea0057f0a2a79907df67504b146e719cda46e
MrZed <balakirevdanil2233@mail.ru>
2022-09-11 22:58:38 +0300

    seve_all2

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-
+from config import CANDIDATES_DATA_LOCATION
 from candidates_manager import CandidatesManager
 
 app = Flask(__name__)
 
-manager = CandidatesManager("candidates.json")
+manager = CandidatesManager(CANDIDATES_DATA_LOCATION)
 
 
 @app.route('/')
@@ -16,10 +16,33 @@ def page_index():
 
 @app.route('/candidate/<int:pk>')
 def page_single_candidates(pk):
+
     candidates = manager.get_candidate(pk)
     return render_template('single.html', candidates=candidates)
 
 
+@app.route('/search/<candidate_name>')
+def page_single_for_candidates(candidate_name):
+
+    candidates = manager.get_candidates_by_name(candidate_name)
+    candidates_len = len(candidates)
+
+    return render_template('search.html',
+                           candidates=candidates,
+                           candidates_len=candidates_len
+                           )
+
+
+@app.route('/skill/<skill_name>')
+def page_show_by_skill(skill_name):
+    candidates = manager.get_candidates_by_skill(skill_name)
+    candidates_len = len(candidates)
+
+    return render_template('skill.html',
+                           candidates=candidates,
+                           candidates_len=candidates_len,
+                           skill_name=skill_name
+                           )
 
 
 if __name__ == '__main__':
